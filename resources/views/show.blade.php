7452625b03ab2bdd55e3964eab7020e502da8617
Sanzhar <tolendy017@gmail.com>
2022-11-09 23:48:39 +0600

    first commit

---
+++ resources/views/show.blade.php
@@ -0,0 +1,21 @@
+@extends("layout")
+@section("title", "User "  .$user->name)
+@section("content")
+    <a type="button" class="btn btn-secondary " href="{{route("users.index")}}">Back</a>
+    <div class="card mt-4" style="width: 18rem;">
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">Id: {{$user->id}}</li>
+            <li class="list-group-item">Name: {{$user->name}}</li>
+            <li class="list-group-item">Email: {{$user->email}}</li>
+            <li class="list-group-item">Created: {{$user->created_at->format("d/m/y H :i:s")}}</li>
+            <li class="list-group-item">Updated: {{$user->updated_at->format("d/m/y H :i:s")}}</li>
+        </ul>
+    </div>
+    <form class="mt-3" method="POST" action="{{route("users.destroy", $user)}}">
+        <a type="button" class="btn btn-warning" href="{{route("users.edit", $user)}}">Edit</a>
+        @csrf
+        @method("DELETE")
+        <button class="btn btn-danger" type="submit">Delete</button>
+    </form>
+
+@endsection
